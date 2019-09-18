32551cda35f1bcd0d5ae0e24b98d6f0b76cdbb7a
Reynaldo Limoli <reynaldo.limoli@gmail.com>
2019-09-18 16:22:43 -0300

    end of the first part

---
+++ vaga.rb
@@ -0,0 +1,26 @@
+class Vaga
+    attr_reader :descricao
+    attr_writer :descricao
+    attr_accessor :titulo, :candidatos
+
+    #construtor
+    def initialize(titulo, descricao)
+        @titulo = titulo
+        @descricao = descricao
+        @ativa = false
+        @candidatos = []
+    end
+
+    def ativar!
+        self.ativa = true 
+    end
+
+    def ativa?
+        ativa()
+    end
+
+    private
+
+    attr_accessor :ativa
+    
+end
